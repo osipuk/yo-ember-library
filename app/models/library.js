fbfb157272a9e520343592cc9292d9138de61483
David Zernik <david@yogaglo.com>
2018-03-11 15:58:26 -0700

    removing old code, updating libary model.

--- app/models/library.js
+++ app/models/library.js
@@ -1,27 +1,10 @@
 import { notEmpty } from '@ember/object/computed';
 import DS from 'ember-data';
-import Faker from 'faker';
 
 export default DS.Model.extend({
 
   name: DS.attr('string'),
-  // address: DS.attr('string'),
-  // phone: DS.attr('string'),
-
-  // books: DS.hasMany('book', { inverse: 'library', async: true }),
-
-  isValid: notEmpty('name'),
-
-  randomize() {
-    this.set('name', Faker.company.companyName() + ' Library');
-    this.set('address', this._fullAddress());
-    this.set('phone', Faker.phone.phoneNumber());
-
-    // If you would like to use in chain.
-    return this;
-  },
-
-  _fullAddress() {
-    return `${Faker.address.streetAddress()}, ${Faker.address.city()}`;
-  }
+  soundType: DS.attr('string'),
+  audioLocation: DS.attr('string'),
+  isValid: notEmpty('name')
 });
