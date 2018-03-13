e08c4da7efc05e2c736fc75c57b84ade716ec51c
David Zernik <david@yogaglo.com>
2018-03-13 14:14:13 -0700

    add add sound section below each library-item.

--- app/controllers/libraries/new.js
+++ app/controllers/libraries/new.js
@@ -3,7 +3,6 @@ import Controller from '@ember/controller';
 export default Controller.extend({
     actions: {
         saveLibrary(newLibrary) {
-        console.log('do it!', newLibrary);
           newLibrary.save().then(() => this.transitionToRoute('libraries'));
         }
     }
