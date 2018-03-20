9f3b7d4f391531c589d8d0b1454ba40890b6290e
David Zernik <david@yogaglo.com>
2018-03-19 18:30:56 -0700

    starting to mess with closure actions.

--- app/controllers/libraries/index.js
+++ app/controllers/libraries/index.js
@@ -8,24 +8,31 @@ export default Controller.extend({
   filter: '',
   letter: '',
   limit: 'all',
+  sources: [],
 
   limitAll: equal('limit', 'all'),
 
   filteredList: computed('model.@each.name', 'filter', function() {
 
-    let results = this.get('model');
-    const query = this.get('filter');
+	let results = this.get('model');
+	const query = this.get('filter');
 
-    if (query) {
-      // One of the best regular expression website: http://www.regexr.com/
-      // Split the query at spaces and join them to get like this: /(word1)+.*(word2)+.*(word3)+.*/ig
-      const regexString = '(' + query.split(' ').join(')+.*(') + ')+.*';
-      // i: case insensitive, g: global
-      const regex = new RegExp(regexString, 'ig');
+	if (query) {
+		// One of the best regular expression website: http://www.regexr.com/
+		// Split the query at spaces and join them to get like this: /(word1)+.*(word2)+.*(word3)+.*/ig
+		const regexString = '(' + query.split(' ').join(')+.*(') + ')+.*';
+		// i: case insensitive, g: global
+		const regex = new RegExp(regexString, 'ig');
 
-      results = results.filter((item) => item.get('name').match(regex));
-    }
+		results = results.filter((item) => item.get('name').match(regex));
+	}
 
-    return results.sortBy('name');
-  })
+	return results.sortBy('name');
+}),
+
+	actions: {
+		addSound (src) {
+			sources.push(src);
+		}
+	}
 });
